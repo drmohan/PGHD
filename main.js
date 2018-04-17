0de8f4449fa1afb1c52221de52270330f7b95c0f
Divya <drmohan@andrew.cmu.edu>
2018-04-17 14:17:09 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,189 @@
+/*
+The purpose of this demo is to demonstrate how multiple charts on the same page
+can be linked through DOM and Highcharts events and API methods. It takes a
+standard Highcharts config with a small variation for each data set, and a
+mouse/touch event handler to bind the charts together.
+*/
+
+
+
+/**
+ * In order to synchronize tooltips and crosshairs, override the
+ * built-in events with handlers defined on the parent element.
+ */
+    $('#container').bind('mousemove touchmove touchstart', function(e) {
+        var chart,
+            point,
+            i;
+            
+        // Find coordinates within the chart. In case charts are side by side,
+        // use the chart we are hovering.
+        for (i = 0; i < Highcharts.charts.length; i = i + 1) {
+            chart = Highcharts.charts[i];
+            e = chart.pointer.normalize(e); 
+            if (e.chartX > chart.xAxis[0].pos && e.chartX < chart.xAxis[0].pos + chart.xAxis[0].len) {
+            	break;
+            }
+		}
+        for (i = 0; i < Highcharts.charts.length; i = i + 1) {
+            chart = Highcharts.charts[i];
+            point = chart.series[0].searchPoint(e, true); // Get the hovered point
+            if (point) {
+                point.onMouseOver(); // Show the hover marker
+                chart.tooltip.refresh(point); // Show the tooltip
+                chart.xAxis[0].drawCrosshair(e, point); // Show the crosshair
+            }
+        }
+    });
+/**
+ * Override the reset function, we don't need to hide the tooltips and
+ * crosshairs.
+ */
+Highcharts.Pointer.prototype.reset = function () {
+    return undefined;
+};
+
+/**
+ * Highlight a point by showing tooltip, setting hover state and draw crosshair
+ */
+Highcharts.Point.prototype.highlight = function (event) {
+    event = this.series.chart.pointer.normalize(event);
+    this.onMouseOver(); // Show the hover marker
+    this.series.chart.tooltip.refresh(this); // Show the tooltip
+    this.series.chart.xAxis[0].drawCrosshair(event, this); // Show the crosshair
+};
+
+/**
+ * Synchronize zooming through the setExtremes event handler.
+ */
+function syncExtremes(e) {
+    var thisChart = this.chart;
+
+    if (e.trigger !== 'syncExtremes') { // Prevent feedback loop
+        Highcharts.each(Highcharts.charts, function (chart) {
+            if (chart !== thisChart) {
+                if (chart.xAxis[0].setExtremes) { // It is null while updating
+                    chart.xAxis[0].setExtremes(
+                        e.min,
+                        e.max,
+                        undefined,
+                        false,
+                        { trigger: 'syncExtremes' }
+                    );
+                }
+            }
+        });
+    }
+}
+
+// Get the data. The contents of the data file can be viewed at
+$.getJSON(
+    // 'https://cdn.rawgit.com/highcharts/highcharts/057b672172ccc6c08fe7dbb27fc17ebca3f5b770/samples/data/activity.json',
+    "data.json",
+    function (activity) {
+        $.each(activity.datasets, function (i, dataset) {
+
+        	var enabled;
+        	if(i == 0 ) {
+        		enabled = true;
+        	} else {
+        		enabled = false;
+        	}
+
+            // Add X values
+            dataset.data = Highcharts.map(dataset.data, function (val, j) {
+                return [Date.parse(activity.xData[j]), val];
+            });
+
+            $('<div class="chart">')
+                .appendTo('#container')
+                .highcharts({
+                    chart: {
+                        marginLeft: 60, // Keep all charts left aligned
+                        spacingTop: 70,
+                        spacingBottom: 40,
+                        style: {
+                            fontFamily: 'Open Sans'
+                        }
+                    },
+                    title: {
+                        text: dataset.name,
+                        align: 'left',
+                        margin: 0,
+                        x: 50,
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    rangeSelector: {
+                    	enabled: enabled,
+                    	floating: true,
+			            y: -90,
+			            verticalAlign: 'top',
+                    	buttons: [{
+						    type: 'week',
+						    count: 1,
+						    text: '1w'
+						}, {
+						    type: 'week',
+						    count: 2,
+						    text: '2w'
+						}, {
+						    type: 'all',
+						    text: 'All'
+						}],
+
+			        },
+                    credits: {
+                        enabled: false
+                    },
+                    legend: {
+                        enabled: false
+                    },
+                    xAxis: {
+                        crosshair: true,
+                        events: {
+                            setExtremes: syncExtremes
+                        },
+	                    type: 'datetime',
+						labels: {
+						   	format: '{value:%m/%d}'
+	                	},
+	                },
+                    yAxis: {
+                        title: {
+                            text: dataset.unit
+                        }
+                    },
+                    tooltip: {
+                        positioner: function () {
+                            return {
+                                // right aligned
+                                x: this.chart.chartWidth - this.label.width,
+                                y: 60 // align to title
+                            };
+                        },
+                        borderWidth: 0,
+                        backgroundColor: 'none',
+                        pointFormat: '{point.y}',
+                        headerFormat: '',
+                        shadow: false,
+                        style: {
+                            fontSize: '18px'
+                        },
+                        valueDecimals: dataset.valueDecimals
+                    },
+                    series: [{
+                        data: dataset.data,
+                        name: dataset.name,
+                        type: dataset.type,
+                        color: Highcharts.getOptions().colors[0],
+                        fillOpacity: 0.3,
+                        tooltip: {
+                            valueSuffix: ' ' + dataset.unit
+                        }
+                    }]
+                });
+        });
+    }
+);
